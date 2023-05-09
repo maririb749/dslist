ff09fc29cafcea579cae24dbac3eecee5606dca7
Mariana Santos <maririb51@gmail.com>
2023-05-09 11:11:56 +0100

    Project created

---
+++ backend/src/test/java/com/mariana/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariana.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
